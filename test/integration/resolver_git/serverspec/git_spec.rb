d019a2c1d0881f3a1114b7d8be0e2343e1f422c1
Adham Helal <aa@hellofresh.com>
2016-02-23 13:32:09 +0100

    Add git test

---
+++ test/integration/resolver_git/serverspec/git_spec.rb
@@ -0,0 +1,49 @@
+require 'serverspec'
+
+# Required by serverspec
+set :backend, :exec
+
+describe file('/opt/manage/deployment') do
+    it { should exist }
+    it { should be_directory }
+    it { should be_mode 755 }
+    it { should be_owned_by 'deploy' }
+    it { should be_grouped_into 'master' }
+end
+
+describe file('/opt/manage/deployment/src') do
+    it { should exist }
+    it { should be_directory }
+    it { should be_mode 755 }
+    it { should be_owned_by 'deploy' }
+    it { should be_grouped_into 'master' }
+end
+
+describe file('/opt/manage/deployment/src/fdbbc5556e177a831dc0e1fb1dd36bf0e8a9bec2') do
+    it { should exist }
+    it { should be_directory }
+    it { should be_mode 755 }
+    it { should be_owned_by 'deploy' }
+    it { should be_grouped_into 'master' }
+end
+
+describe file('/opt/manage/deployment/src/fdbbc5556e177a831dc0e1fb1dd36bf0e8a9bec2/.git') do
+    it { should_not exist }
+end
+
+describe file('/opt/manage/deployment/repo') do
+    it { should exist }
+    it { should be_directory }
+    it { should be_mode 755 }
+    it { should be_owned_by 'deploy' }
+    it { should be_grouped_into 'master' }
+end
+
+describe file('/opt/manage/deployment/repo/.git') do
+    it { should exist }
+end
+
+describe file('/opt/manage/deployment/current') do
+  it { should be_symlink }
+  it { should be_linked_to '/opt/manage/deployment/src/fdbbc5556e177a831dc0e1fb1dd36bf0e8a9bec2' }
+end
