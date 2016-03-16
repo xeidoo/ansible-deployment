c77ec22f9e418fa79afaffbd0e96eaaabaea8b99
Adham Helal <aa@hellofresh.com>
2016-03-16 13:33:01 +0100

    Update tests to include S3 hash and file features

---
+++ test/integration/resolver_s3_hash/serverspec/resolver_hash_spec.rb
@@ -0,0 +1,40 @@
+require 'serverspec'
+
+# Required by serverspec
+set :backend, :exec
+
+git_hash = "f4c25aaf062ab8d1a9079e7b9f9b86a71daa8f2a"
+
+describe file('/opt/manage/test/artifacts') do
+    it { should exist }
+    it { should be_directory }
+    it { should be_mode 750 }
+    it { should be_owned_by 'deploy' }
+    it { should be_grouped_into 'master' }
+end
+
+describe file("/opt/manage/test/artifacts/test-#{git_hash}.tar.gz") do
+    it { should exist }
+    it { should be_file }
+    it { should be_owned_by 'deploy' }
+    it { should be_grouped_into 'master' }
+end
+
+describe file("/opt/manage/test/src/#{git_hash}") do
+    it { should exist }
+    it { should be_owned_by 'deploy' }
+    it { should be_mode 755 }
+    it { should be_grouped_into 'master' }
+end
+
+describe file("/opt/manage/test/src/#{git_hash}/#{git_hash}") do
+    it { should exist }
+    it { should be_owned_by 'deploy' }
+    it { should be_mode 755 }
+    it { should be_grouped_into 'master' }
+end
+
+describe file("/opt/manage/test/current") do
+  it { should be_symlink }
+  it { should be_linked_to "/opt/manage/test/src/#{git_hash}" }
+end
