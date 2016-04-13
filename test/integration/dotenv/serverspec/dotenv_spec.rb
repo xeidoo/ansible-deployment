fdd09d1a4bccf9f716c0b438cd3b353fecdc6d03
Adham Helal <aa@hellofresh.com>
2016-04-13 14:12:35 +0200

    dotenv code and tests

---
+++ test/integration/dotenv/serverspec/dotenv_spec.rb
@@ -0,0 +1,19 @@
+require 'serverspec'
+
+# Required by serverspec
+set :backend, :exec
+
+
+dotenv_file_content="""# Ansible managed, Don't modify manually
+
+myvar1=\"True\"
+myvar2=\"False\""""
+
+describe file('/tmp/envfile.txt') do
+    it { should exist }
+    it { should be_file }
+    it { should be_mode 600 }
+    it { should be_owned_by 'deploy' }
+    it { should be_grouped_into 'master' }
+    its(:content) { should match dotenv_file_content }
+end
