be652472904f777db770312238022f34bb8d57e0
Adham Helal <aa@hellofresh.com>
2016-09-20 14:46:04 +0200

    fix missing curl

--- test/ansible-setup.sh
+++ test/ansible-setup.sh
@@ -7,6 +7,7 @@ SETUP_VERSION="v0.0.3"
 #SETUP_VERBOSITY="vv"
 
 
+
 ## Install Ansible 2.0
 ANSIBLE_VERSIONS[0]="2.0.2.0"
 INSTALL_TYPE[0]="pip"
@@ -20,5 +21,11 @@ ANSIBLE_LABEL[0]="latest"
 # Whats the default version
 ANSIBLE_DEFAULT_VERSION="v2"
 
-#
-. $AVM_SETUP_PATH
+## Create a temp dir
+filename=$( echo ${0} | sed 's|/||g' )
+my_temp_dir="$(mktemp -dt ${filename}.XXXX)"
+
+curl -s https://raw.githubusercontent.com/ahelal/avm/${SETUP_VERSION}/setup.sh -o $my_temp_dir/setup.sh
+
+## Run the setup
+. $my_temp_dir/setup.sh
