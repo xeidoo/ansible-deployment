93f718c790ce1d64d79716cb17e662a8101abd7e
Adham Helal <aa@hellofresh.com>
2016-09-20 14:51:09 +0200

    fix setup index

--- test/ansible-setup.sh
+++ test/ansible-setup.sh
@@ -6,17 +6,15 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 SETUP_VERSION="v0.0.3"
 #SETUP_VERBOSITY="vv"
 
-
-
 ## Install Ansible 2.0
 ANSIBLE_VERSIONS[0]="2.0.2.0"
 INSTALL_TYPE[0]="pip"
 ANSIBLE_LABEL[0]="v2"
 
 ## Install Ansible stable-2.0
-ANSIBLE_VERSIONS[0]="devel"
-INSTALL_TYPE[0]="git"
-ANSIBLE_LABEL[0]="latest"
+ANSIBLE_VERSIONS[1]="devel"
+INSTALL_TYPE[1]="git"
+ANSIBLE_LABEL[1]="latest"
 
 # Whats the default version
 ANSIBLE_DEFAULT_VERSION="v2"
