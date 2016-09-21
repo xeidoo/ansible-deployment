9f2e68db1cea6f05ed91b9b19b9fe44f0089e682
Italo Lelis de Vietro <italolelis@gmail.com>
2016-09-21 16:06:06 +0200

    Fixed download source comparision

--- library/gh_release.py
+++ library/gh_release.py
@@ -95,7 +95,7 @@ class GithubReleases(object):
 
     def download(self, release):
         # Source download
-        if self.download_source is not None and self.download_source is not "None":
+        if self.download_source is not None and self.download_source != "None":
             return release.archive("tarball", self.dest)
 
         # Look at assets
