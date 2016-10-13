015de2b3dc6215959af943bdc1639d82f14b2de9
Robert Kel <rke@hellofresh.com>
2016-10-13 17:35:40 +0200

    Adds Semantic versioning - github releases

--- library/delete_old_deployments.py
+++ library/delete_old_deployments.py
@@ -11,7 +11,7 @@ description:
 options:
   deployment_path:
     description:
-      - Path of deployment dir 
+      - Path of deployment dir
     required: true
     default: null
   keep_last:
@@ -26,11 +26,32 @@ EXAMPLES = '''
 - delete_old_deployments: path=/deployments/app/ keep_last=3
 '''
 
+
+def is_valid_version(version):
+    parsed_version = parse_version(version)
+    if type(parsed_version) == SetuptoolsVersion:
+        return version
+    else:
+        return None
+
 def exec_command(module, command, cwd=None):
     rc, out, err = module.run_command(command, cwd=cwd, use_unsafe_shell=True)
     if rc != 0:
         module.fail_json(msg="Failed to issue command '%s' because stdout='%s' stderr='%s'" % (command, out, err))
     return out
+
+def get_git_hash_dir(module, deployment_dir):
+    command = "find . -maxdepth 1 -type d -exec printf '{}\n' \; | awk 'length==42' | sed 's|./||'"
+    hash_dirs = exec_command(module, command, deployment_dir)
+    hash_dirs = hash_dirs.split("\n")
+    return hash_dirs
+
+def get_sem_ver_dir(module, deployment_dir):
+    command = "find . -maxdepth 1 -type d -exec printf '{}\n' \; | sed 's/.\|.\///'"
+    hash_dirs = exec_command(module, command, deployment_dir)
+    hash_dirs = hash_dirs.split("\n")
+
+    return map(is_valid_version, hash_dirs)
 
 def main():
     changed = False
@@ -40,21 +61,25 @@ def main():
         argument_spec = dict(
             path      = dict(required=True, type='str'),
             keep_last = dict(required=True, type='int'),
+            use_semantic_versioning = dict(default=False, required=False, type='bool'),
         ),
     )
-    deployment_dir    = module.params['path']
-    keep_last         = module.params['keep_last']
-
+    deployment_dir      = module.params['path']
+    keep_last           = module.params['keep_last']
+    semantic_versioning = module.params['use_semantic_versioning']
     # keep deployment info
     list_deployment = []
     broken_deployment_dir = []
-    command = "find . -maxdepth 1 -type d -exec printf '{}\n' \; | awk 'length==42' | sed 's|./||'"
-    hash_dirs = exec_command(module, command, deployment_dir)
-    hash_dirs = hash_dirs.split("\n")
+
+    if semantic_versioning:
+        hash_dirs = get_sem_ver_dir(module, deployment_dir)
+    else:
+        hash_dirs = get_git_hash_dir(module, deployment_dir)
+
     ## Remove empty lists
-    hash_dirs = filter(None, hash_dirs) 
+    hash_dirs = filter(None, hash_dirs)
     for hash_dir in hash_dirs:
-        # Fet earliest date of deployment 
+        # Fet earliest date of deployment
         glob_path = os.path.join(deployment_dir, hash_dir,  "*.deployment.date")
         deployment_dates = glob.glob(glob_path)
         # Sort in case we have mutli deployment. We will take the oldest 1
@@ -74,7 +99,7 @@ def main():
         # Get only items that will be deleted
         list_deployment = list_deployment[0:deployments2deleted]
         for dir_2_delete in list_deployment:
-            changed = True 
+            changed = True
             # Remove any symlinks first inside the dir
             remove_link_cmd = "find %s -maxdepth 1 -type l -exec readlink -f {} \; | xargs rm -f" % os.path.join(deployment_dir,dir_2_delete.get("hash_dir"))
             exec_command(module, remove_link_cmd, deployment_dir)
@@ -99,6 +124,7 @@ import glob
 import os.path
 import subprocess
 from ansible.module_utils.basic import *
+from pkg_resources import parse_version, SetuptoolsVersion
 if __name__ == '__main__':
     main()
 
